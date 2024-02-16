9b91a4eed7884486952ddc8648987e0944e64a12
Carlos <carlosedu4923@gmail.com>
2024-02-16 17:45:50 -0300

    PetLife website project created

---
+++ index.js
@@ -0,0 +1,17 @@
+var elementosDuvida = document.querySelectorAll(".duvida")
+
+elementosDuvida.forEach(function (duvida) {
+    duvida.addEventListener('click', function () {
+        duvida.classList.toggle('duvida-aberta')
+    })
+})
+
+//* function nome(argumento) {Conteúdo da função} */
+
+//? thi iiei
+
+//* eeieue */
+
+//TODO EDGHGKKJ
+
+//! 3GGCHJ NOT
